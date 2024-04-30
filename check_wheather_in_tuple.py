050605e3a49f91fc60f1d548e5a154a073fda2cd
unknown <sreesreekanth061@gmail.com>
2024-04-30 12:44:44 +0530

    check wheather tuple

---
+++ check_wheather_in_tuple.py
@@ -0,0 +1,14 @@
+
+#10. Write a  Python program to check whether an element exists within a tuple.
+
+
+tz=('s','r',3,'e','k','a','t','h')
+
+print('r' in tz)
+
+print(3 in tz)
+print('n' in tz)
+
+
+
+
