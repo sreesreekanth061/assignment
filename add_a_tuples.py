628a5ca31738e87b8065b69fd5ff0a633d764737
unknown <sreesreekanth061@gmail.com>
2024-04-30 12:25:40 +0530

    add tuples file program

---
+++ add_a_tuples.py
@@ -0,0 +1,9 @@
+
+#5. Write a Python program to add an item to a tuple.
+
+tuplex2 = (4,6,2,8,3,1)
+print(tuplex2)
+tuplex2 = tuplex2 +(9,)
+print(tuplex2)
+tuplex2 = tuplex2[:5]+(20,30,40)+tuplex2[:4]
+print(tuplex2)
