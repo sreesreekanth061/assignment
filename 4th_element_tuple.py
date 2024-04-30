95070bd3aa5a54ba096a01665f1348529ddac7ad
unknown <sreesreekanth061@gmail.com>
2024-04-30 12:51:22 +0530

    4th element

---
+++ 4th_element_tuple.py
@@ -0,0 +1,12 @@
+
+#7. Write a Python program to get the 4th element from the last element of a tuple.
+
+tp=('s','r','e','e','k',3,'a','n','t','h')
+print(tp)
+
+item=tp[3]
+
+print(item)
+
+item1=tp[-1]
+print(item1)
